65c9291b14300f732aedc12cfa74d31af15fa9f8
james <monaco.james18@gmail.com>
2020-11-05 15:12:57 -0600

    new cats, and loading text added

--- public/events.js
+++ public/events.js
@@ -2,8 +2,26 @@
 window.addEventListener("DOMContentLoaded", async (event) => {
   const res = await fetch("/kitten/image"); // result is currently a promise.
   const json = await res.json();
-  debugger;
+  //debugger;
   if (res.ok) {
     document.querySelector(`.cat-pic`).setAttribute("src", json.src)
   };
+
+})
+
+document.querySelector("#new-pic").addEventListener("click", (event) =>{
+  event.preventDefault();
+  fetch("/kitten/image") // result is currently a promise.
+    .then (unresolvedFetch => {
+      document.querySelector(".loader").innerHTML = "Loading..."
+      //  debugger;
+      return unresolvedFetch;
+    })
+    .then (unresolvedFetch => {
+      return unresolvedFetch.json();
+    })
+    .then (resolvedFetch => {
+        document.querySelector(`.cat-pic`).setAttribute("src", resolvedFetch.src);
+        document.querySelector(".loader").innerHTML = ""
+    })
 })
